ebf54ab6333e857a3c2de9938bba4702afcaad9c
Julian Kuhlmann <jlnkuhlmann@gmail.com>
2016-01-30 18:40:24 +0100

    Initial commit.

---
+++ desktop/src/net/cosaa/dicrocoelium/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package net.cosaa.dicrocoelium.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import net.cosaa.dicrocoelium.Dicrocoelium;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Dicrocoelium(), config);
+	}
+}
